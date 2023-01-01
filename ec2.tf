8a4628e34a2f7e8f17516ae416b4e11362ef59e8
satzwebio <57879063+satzwebio@users.noreply.github.com>
2023-01-01 13:13:14 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,22 +1,18 @@
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "YOUR-ACCESS-KEY"
-  secret_key = "YOUR-SECRET-KEY"
-}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-module "ec2_cluster" {
-  source                 = "terraform-aws-modules/ec2-instance/aws"
-  version                = "~> 2.0"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-  name                   = "my-cluster"
-  instance_count         = 1
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  ami                    = "ami-0d6621c01e8c2de2c"
-  instance_type          = "t2.micro"
-  subnet_id              = "subnet-4dbfb206"
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    Terraform   = "true"
+    Terraform = "true"
     Environment = "dev"
   }
 }
